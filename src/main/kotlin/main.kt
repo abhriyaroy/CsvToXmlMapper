f856650d0bddb725d6278f85105491290c35be87
Abhriya <abhriya.roy@biofourmis.com>
2021-09-25 10:26:59 +0530

    Add comments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,14 +12,14 @@ import java.nio.file.Paths
 fun main(args: Array<String>) {
     val rfc4180Parser = RFC4180ParserBuilder().build()
     val csvReaderBuilder = CSVReaderBuilder(
-        FileReader("sample_translations.csv")
+        FileReader("sample_translations.csv") // Replace with the name of your csv file pasted at the project root level
     )
 
     val reader = csvReaderBuilder
         .withCSVParser(rfc4180Parser)
         .build()
 
-    val path = Paths.get("strings.xml")
+    val path = Paths.get("strings.xml") // Paste yout strings.xml file at the project root level. Overwrite the existing file if you're asked while pasting
     val charset: Charset = StandardCharsets.UTF_8
 
     var content = String(Files.readAllBytes(path), charset)
@@ -29,9 +29,9 @@ fun main(args: Array<String>) {
             content = content.replace(">" + it[it.size - 2] + "<", ">" + it[it.size - 1] + "<")
         }
     }
-//    Files.write(path, content.toByteArray(charset))
+
     try {
-        FileWriter("strings_translated.xml").use { writer ->
+        FileWriter("strings_translated.xml").use { writer -> // This is the outfile and you can find it in the project root level
             writer.write(content)
         }
     } catch (e: IOException) {
